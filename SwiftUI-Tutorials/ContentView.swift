9419c78ee7369b5d5071478bac45d3cf06bdb8f8
Anuj Soni <anujsoni10001@gmail.com>
2022-03-09 17:54:29 +0530

    Initial Commit

---
+++ SwiftUI-Tutorials/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Tutorials
+//
+//  Created by Anuj Soni on 09/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Tutorials/SwiftUI_TutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TutorialsApp.swift
+//  SwiftUI-Tutorials
+//
+//  Created by Anuj Soni on 09/03/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
